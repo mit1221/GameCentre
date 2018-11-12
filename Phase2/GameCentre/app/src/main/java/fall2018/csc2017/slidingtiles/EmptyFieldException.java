abfd9f2372e4a90696cb0636d2cafdd135ffc28a
David Lim <davidhslim6793@gmail.com>
2018-11-12 15:00:13 -0500

    Moved files

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/EmptyFieldException.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/EmptyFieldException.java
@@ -1,9 +1,9 @@
 package fall2018.csc2017.slidingtiles;
 
 
-class EmptyFieldException extends Exception {
+public class EmptyFieldException extends Exception {
 
-    EmptyFieldException(String message) {
+    public EmptyFieldException(String message) {
         super(message);
     }
 }
--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/UserException.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/UserException.java
@@ -4,9 +4,9 @@ package fall2018.csc2017.slidingtiles;
  * For throwing exceptions when username already exists
  */
 
-class UserException extends Exception {
+public class UserException extends Exception {
 
-    UserException(String message) {
+    public UserException(String message) {
         super(message);
     }
 }
