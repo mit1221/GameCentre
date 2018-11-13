c9116f161b80f7ee6c9c25eb75f77409be30d7e7
Mit <mit_rupang@yahoo.ca>
2018-11-13 15:44:29 -0500

    Added activities for the other games and fixed GameCenter layout

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/sudoku/SudokuMenuActivity.java
@@ -0,0 +1,15 @@
+package fall2018.csc2017.sudoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fall2018.csc2017.slidingtiles.R;
+
+public class SudokuMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudoku_menu);
+    }
+}
