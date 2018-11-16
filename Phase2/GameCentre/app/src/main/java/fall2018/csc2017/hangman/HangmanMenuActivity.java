919b8cd1a2b767ed0b9f42d07cc5db66d61de948
jasonkclee <25747459+jasonkclee@users.noreply.github.com>
2018-11-16 14:19:08 -0500

    Created new HangmanState class. Updated MenuActivity with some buttons

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/hangman/HangmanMenuActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/hangman/HangmanMenuActivity.java
@@ -2,14 +2,45 @@ package fall2018.csc2017.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import fall2018.csc2017.User;
 import fall2018.csc2017.slidingtiles.R;
 
+/**
+ * Menu Activity for the Hangman game
+ */
 public class HangmanMenuActivity extends AppCompatActivity {
+
+    /**
+     * The current user
+     */
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman_menu);
+
+        // retrieve the logged in user
+        user = (User)this.getIntent().getSerializableExtra("User");
     }
+
+    /**
+     * Handle what happens when user clicks New Game
+     * @param view - clicked button
+     */
+    public void onBtnNewGameClick(View view){
+
+    }
+
+    /**
+     * Handle what happens when user clicks New Game
+     * @param view - clicked button
+     */
+    public void onBtnLoadGameClick(View view){
+
+    }
+
+
 }
