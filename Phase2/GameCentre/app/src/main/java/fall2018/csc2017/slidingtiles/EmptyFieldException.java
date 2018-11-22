b2f56a13b55dd1e419b9306986df00cc6837f79d
victor <v.trinh@mail.utoronto.ca>
2018-11-21 22:34:51 -0500

    Added JavaDoc to EmptyFieldException class.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/EmptyFieldException.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/EmptyFieldException.java
@@ -1,8 +1,13 @@
 package fall2018.csc2017.slidingtiles;
 
-
+/**
+ * An EmptyFieldException.
+ */
 public class EmptyFieldException extends Exception {
-
+    /**
+     * A new EmptyFieldException with message message.
+     * @param message
+     */
     public EmptyFieldException(String message) {
         super(message);
     }
