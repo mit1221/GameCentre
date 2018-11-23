e7d4c592685323c12c3f1ac6d216b92810393430
David Lim <davidhslim6793@gmail.com>
2018-11-23 04:21:20 -0500

    Add some tests to BoardTest

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/BoardTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/BoardTest.java
@@ -15,26 +15,56 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import fall2018.csc2017.UndoMoveList;
+
 import static org.junit.Assert.*;
 
 
 public class BoardTest {
 
-    @Test
-    public void testNumTiles() {
-        Board board = mock(Board.class);
-        Tile tile0 = new Tile(0);
-        Tile tile1 = new Tile(1);
-        when(board.getTile(0,0)).thenReturn(tile0);
-        when(board.getTile(0,1)).thenReturn(tile1);
+    /**
+     * Make a set of tiles that are in order.
+     * @return a set of tiles that are in order
+     */
+    private List<Tile> makeTiles() {
+        List<Tile> tiles = new ArrayList<>();
+        final int numTiles = Board.NUM_ROWS * Board.NUM_COLS;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(tileNum + 1, tileNum));
+        }
 
-//        board.swap(0,0,0,1);
+        return tiles;
+    }
+
+
+    @Test
+    public void testMakeMove() {
+
+        Board.NUM_ROWS = Board.NUM_COLS = 4;
+        Board board = new Board(makeTiles());
+        Tile tile0 = board.getTile(0,0);
+        Tile tile1 = board.getTile(0,1);
+        board.makeMove(0,0,0,1);
+
+
+        Assert.assertEquals(board.getTile(0,0), tile1);
+        Assert.assertEquals(board.getTile(0,1), tile0);
+        Assert.assertEquals(board.getMovesMade(), 1);
+
+    }
+
+    @Test
+    public void testUndoMove(){
+        Board.NUM_ROWS = Board.NUM_COLS = 4;
+        Board board = new Board(makeTiles());
+        Tile tile0 = board.getTile(0,0);
+        Tile tile1 = board.getTile(0,1);
+        board.makeMove(0,0,0,1);
+        board.undoLastMove();
 
         Assert.assertEquals(board.getTile(0,0), tile0);
         Assert.assertEquals(board.getTile(0,1), tile1);
-
-//        verify(board).swap(0,0,0,1);
-
+        Assert.assertEquals(board.getMovesMade(), 2);
     }
 
 }
