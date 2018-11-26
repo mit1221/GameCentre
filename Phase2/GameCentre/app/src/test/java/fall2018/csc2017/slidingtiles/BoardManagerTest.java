51ddfa219bc82e8e43a0ba528a2cb6a2c4b174ba
David Lim <davidhslim6793@gmail.com>
2018-11-26 12:49:40 -0500

    Add tests on BoardManagerTest

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/BoardManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/BoardManagerTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 import fall2018.csc2017.UndoMoveList;
 
@@ -50,9 +51,52 @@ public class BoardManagerTest {
         boardManager = new BoardManager(board);
     }
 
+    /**
+     * Make a solved Board.
+     * @param size size of the board (number of rows/cols).
+     */
+    private void setUpIncorrect(int size) {
+        Board.NUM_ROWS = Board.NUM_COLS = size;
+        List<Tile> tiles = makeTiles();
+        Collections.reverse(tiles);
+        Board board = new Board(tiles);
+        boardManager = new BoardManager(board);
+    }
+
 
     @Test
     public void testSolved(){
+        setUpCorrect(3);
+        assertTrue(boardManager.puzzleSolved());
+    }
+
+    //test not solved collections swap
+
+    @Test
+    public void testNotSolved(){
+        setUpIncorrect(4);
+        assertFalse(boardManager.puzzleSolved());
+    }
+
+    @Test
+    public void testIsValidTap(){
+        setUpCorrect(5);
+        assertTrue(boardManager.isValidTap(23));
+        assertTrue(boardManager.isValidTap(19));
+        assertFalse(boardManager.isValidTap(18));
+        assertFalse(boardManager.isValidTap(0));
+        assertFalse(boardManager.isValidTap(24));
 
     }
+
+    @Test
+    public void testTouchMove(){
+        setUpIncorrect(4);
+        Tile tile0 = boardManager.getBoard().getTile(0,0);
+        Tile tile1 = boardManager.getBoard().getTile(0,1);
+        boardManager.touchMove(1);
+        assertEquals(boardManager.getBoard().getTile(0,0), tile1);
+        assertEquals(boardManager.getBoard().getTile(0,1), tile0);
+    }
+
 }
