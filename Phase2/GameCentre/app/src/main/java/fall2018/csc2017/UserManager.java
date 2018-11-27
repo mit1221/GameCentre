7bf72e112a60c105f8292a188bc194135f899b76
victor <v.trinh@mail.utoronto.ca>
2018-11-27 14:15:51 -0500

    Changed usermanager to allow usage of alternative directories.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/UserManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/UserManager.java
@@ -24,42 +24,68 @@ public class UserManager implements Serializable {
     private static String defaultDir = "users.txt";
 
     /**
-     * Register a new user with the given userName and password.
+     * Register a new user with the given userName and password to directory dir.
      *
      * @param userName the new user's name
      * @param password the new user's password(hashed)
+     * @param dir the directory to register to
      * @return the newly registered user
      */
-    public static User registerNewUser(String userName, String password, Context c) throws
+    public static User registerNewUser(String userName, String password, Context c, String dir) throws
             UserException, EmptyFieldException {
         if (userName.equals("") || password.equals("")) {
             throw new EmptyFieldException("Username or password cannot be empty");
         }
-        if (userExists(userName, c)) { // userName already exists in the stored users
+        if (userExists(userName, c, dir)) { // userName already exists in the stored users
             throw new UserException("User with userName " + userName + " already exists!");
         } else {
             User newUser = new User(userName, password.hashCode()); // hash the password
             users.add(newUser);
-            saveUsers(c);
+            saveUsers(c, dir);
             return newUser;
         }
     }
 
     /**
+     * Register a new user with the given userName and password to default directory.
+     * @param userName the new user's name
+     * @param password the new user's password(hashed)
+     * @return the newly registered user
+     */
+    public static User registerNewUser(String userName, String password, Context c) throws
+            UserException, EmptyFieldException{
+        return registerNewUser(userName, password, c, defaultDir);
+    }
+
+    /**
+     * Check if user exists in directory dir.
+     * @param userName name of the user to check for.
+     * @param c        context to read files in
+     * @param dir the directory to check in
+     * @return whether the user already exists
+     */
+    public static boolean userExists(String userName, Context c, String dir) {
+        return getUser(userName, c, dir) != null;
+    }
+
+    /**
+     * Check if user exists in default dir.
      * @param userName name of the user to check for.
      * @param c        context to read files in
      * @return whether the user already exists
      */
     public static boolean userExists(String userName, Context c) {
-        return getUser(userName, c) != null;
+        return userExists(userName, c, defaultDir);
     }
 
     /**
+     * Get user from directory dir if user exists.
      * @param userName name of the user to look for.
+     * @param dir the directory to look in
      * @return User object if it exists, null otherwise
      */
-    public static User getUser(String userName, Context c) {
-        loadUsers(c);
+    public static User getUser(String userName, Context c, String dir) {
+        loadUsers(c, dir);
         for (User u : users) {
             if (u.getUserName().equals(userName)) {
                 return u;
@@ -69,13 +95,23 @@ public class UserManager implements Serializable {
     }
 
     /**
+     * Get user from default dir if user exists.
+     * @param userName
+     * @return User object if it exists, null otherwise
+     */
+    public static User getUser(String userName, Context c) {
+        return getUser(userName, c, defaultDir);
+    }
+
+    /**
      * @param userName the username of the user trying to login
      * @param password the password of the user trying to login
      * @param c        the context from which to login.
+     * @param dir       the directory containing the user attempting to log in
      * @return the user if the login was successful, else null.
      */
-    public static User login(String userName, String password, Context c) {
-        loadUsers(c);
+    public static User login(String userName, String password, Context c, String dir) {
+        loadUsers(c, dir);
         for (User u : users) {
             if (u.checkCredentials(userName, password)) {
                 return u;
@@ -84,6 +120,17 @@ public class UserManager implements Serializable {
         return null;
     }
 
+    /**
+     * Version of login() utilizing the default directory.
+     * @param userName the username of the user trying to login
+     * @param password the password of the user trying to login
+     * @param c        the context from which to login.
+     * @return the user if the login was successful, else null.
+     */
+    public static User login(String userName, String password, Context c) {
+        return login(userName, password, c, defaultDir);
+
+    }
     /**
      * Saves users to text file.
      *
@@ -142,12 +189,13 @@ public class UserManager implements Serializable {
     }
 
     /**
-     * Save the user's state; replaces the state if user already exists
+     * Save the user's state; replaces the state if user already exists. Utilize directory dir.
      *
      * @param user User object to save
      * @param c    Context to use for saving files
+     * @param dir   the directory to use
      */
-    public static void saveUserState(User user, Context c) {
+    public static void saveUserState(User user, Context c, String dir) {
         for (int i = 0; i < users.size(); i++) {
             User u = users.get(i);
             if (u.getUserName().equals(user.getUserName())) { // replace existing user data if exists
@@ -156,6 +204,15 @@ public class UserManager implements Serializable {
             }
         }
         users.add(user);
-        saveUsers(c);
+        saveUsers(c, dir);
+    }
+    /**
+     * Save the user's state; replaces the state if user already exists. Utilize default directory.
+     *
+     * @param user User object to save
+     * @param c    Context to use for saving files
+     */
+    public static void saveUserState(User user, Context c) {
+        saveUserState(user, c, defaultDir);
     }
 }
