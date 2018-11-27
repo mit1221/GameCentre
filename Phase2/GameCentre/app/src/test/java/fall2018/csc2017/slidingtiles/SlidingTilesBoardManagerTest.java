403a8ad502d4ca9807a8f94903dd5a9f6f53830c
David Lim <davidhslim6793@gmail.com>
2018-11-27 15:14:08 -0500

    Refactor SlidingTileBoardManagerTest

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingTilesBoardManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingTilesBoardManagerTest.java
@@ -7,13 +7,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
+import android.content.Context;
+import android.widget.Toast;
 
-import fall2018.csc2017.Tile;
+
+import fall2018.csc2017.*;
 
 public class SlidingTilesBoardManagerTest {
 
@@ -21,6 +26,12 @@ public class SlidingTilesBoardManagerTest {
      * The board manager for testing.
      */
     SlidingTilesBoardManager boardManager;
+
+    /**
+     * The movement controller for testing.
+     */
+    MovementController movementController;
+
 
     /**
      * Make a set of tiles that are in order.
@@ -54,7 +65,7 @@ public class SlidingTilesBoardManagerTest {
      */
     private void setUpIncorrect(int size) {
         List<SlidingTile> tiles = makeTiles(size);
-        Collections.reverse(tiles);
+        Collections.swap(tiles, size*size-1, size*size-2);
         SlidingTilesBoard board = new SlidingTilesBoard(size, tiles, 3);
         boardManager = new SlidingTilesBoardManager(board);
     }
@@ -74,23 +85,34 @@ public class SlidingTilesBoardManagerTest {
         assertFalse(boardManager.puzzleSolved());
     }
 
-    @Test
-    public void testIsValidTap() {
-        setUpCorrect(5);
-        assertTrue(boardManager.isValidMove((SlidingTilesMove.createMove(23,boardManager.getBoard()))));
-        assertTrue(boardManager.isValidMove((SlidingTilesMove.createMove(19,boardManager.getBoard()))));
-        assertFalse(boardManager.isValidMove((SlidingTilesMove.createMove(18,boardManager.getBoard()))));
-        assertFalse(boardManager.isValidMove((SlidingTilesMove.createMove(0,boardManager.getBoard()))));
-        assertFalse(boardManager.isValidMove((SlidingTilesMove.createMove(24,boardManager.getBoard()))));
-    }
+//    @Test
+//    public void testIsValidTap() {
+//        setUpCorrect(5);
+//        assertTrue(boardManager.isValidMove((SlidingTilesMove.createMove(23,boardManager.getBoard()))));
+//        assertTrue(boardManager.isValidMove((SlidingTilesMove.createMove(19,boardManager.getBoard()))));
+//        assertFalse(boardManager.isValidMove((SlidingTilesMove.createMove(18,boardManager.getBoard()))));
+//        assertFalse(boardManager.isValidMove((SlidingTilesMove.createMove(0,boardManager.getBoard()))));
+//        assertFalse(boardManager.isValidMove((SlidingTilesMove.createMove(24,boardManager.getBoard()))));
+//    }
 
     @Test
     public void testTouchMove() {
         setUpIncorrect(3);
-        Tile tile0 = boardManager.getBoard().getTile(0, 0);
-        Tile tile1 = boardManager.getBoard().getTile(0, 1);
-        boardManager.touchMove(SlidingTilesMove.createMove(1,boardManager.getBoard()));
-        assertEquals(boardManager.getBoard().getTile(0, 0), tile1);
-        assertEquals(boardManager.getBoard().getTile(0, 1), tile0);
+        Tile tile0 = boardManager.getBoard().getTile(2, 2);
+        Tile tile1 = boardManager.getBoard().getTile(2, 1);
+        boardManager.touchMove(SlidingTilesMove.createMove(8,boardManager.getBoard()));
+        assertEquals(boardManager.getBoard().getTile(2, 2), tile1);
+        assertEquals(boardManager.getBoard().getTile(2, 1), tile0);
+    }
+
+    @Test
+    public void testMove(){
+        setUpCorrect(4);
+        Context context = mock(Context.class);
+        movementController = new MovementController();
+        movementController.setBoardManager(boardManager);
+        movementController.processMove(context, SlidingTilesMove.createMove(14,boardManager.getBoard()));
+        verify(Toast.makeText(context, "YOU WIN!", Toast.LENGTH_SHORT)).show();
+
     }
 }
