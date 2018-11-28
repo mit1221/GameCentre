b9c083c7e4fa2013c4b5be43a1159f9a671011ab
victor <v.trinh@mail.utoronto.ca>
2018-11-27 19:43:00 -0500

    Added documentation for UserTest.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/UserTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/slidingtiles/UserTest.java
@@ -7,13 +7,17 @@ import fall2018.csc2017.User;
 
 public class UserTest {
 
-    public User Setup(){
+    /**
+     * Instantiate a dummy user for testing.
+     * @return a dummy user for testing.
+     */
+    private User setupUser(){
         return new User("user",  "1234".hashCode());
     }
 
     @Test
     public void testInstanceVariables(){
-        User u = Setup();
+        User u = setupUser();
 
         Assert.assertEquals(u.getUserName(), "user");
         Assert.assertEquals(u.getPassword(), "1234".hashCode());
@@ -21,7 +25,7 @@ public class UserTest {
 
     @Test
     public void testSaves(){
-        User u = Setup();
+        User u = setupUser();
         Object save = new Object();
         Assert.assertFalse(u.hasSave(Game.HANGMAN));
         u.setSave(Game.HANGMAN, save);
@@ -31,7 +35,7 @@ public class UserTest {
 
     @Test
     public void testCheckCredentials(){
-        User u = Setup();
+        User u = setupUser();
         Assert.assertTrue(u.checkCredentials("user", "1234"));
         Assert.assertFalse(u.checkCredentials("user", "123"));
         Assert.assertFalse(u.checkCredentials("usr", "1234"));
