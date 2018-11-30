bfed52e6804fd79cf3062d7209b437d21df85443
Mit <mit_rupang@yahoo.ca>
2018-11-29 20:17:50 -0500

    Added gridview for Sudoku

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/sudoku/SudokuGridView.java
@@ -0,0 +1,121 @@
+package fall2018.csc2017.sudoku;
+
+/*
+Adapted from:
+https://github.com/DaveNOTDavid/sample-puzzle/blob/master/app/src/main/java/com/davenotdavid/samplepuzzle/GestureDetectGridView.java
+
+This extension of GridView contains built in logic for handling swipes between buttons
+ */
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import fall2018.csc2017.BoardManager;
+import fall2018.csc2017.Move;
+import fall2018.csc2017.MovementController;
+import fall2018.csc2017.slidingtiles.SlidingTilesMove;
+
+public class SudokuGridView extends GridView {
+    public static final int SWIPE_MIN_DISTANCE = 100;
+    public static final int SWIPE_MAX_OFF_PATH = 100;
+    public static final int SWIPE_THRESHOLD_VELOCITY = 100;
+    private GestureDetector gDetector;
+    private MovementController mController;
+    private boolean mFlingConfirmed = false;
+    private float mTouchX;
+    private float mTouchY;
+    private BoardManager boardManager;
+
+    public SudokuGridView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public SudokuGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public SudokuGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP) // API 21
+    public SudokuGridView(Context context, AttributeSet attrs, int defStyleAttr,
+                          int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(final Context context) {
+        mController = new MovementController();
+        gDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent event) {
+                int position = SudokuGridView.this.pointToPosition
+                        (Math.round(event.getX()), Math.round(event.getY()));
+                Log.d("mytag3", String.valueOf(position));
+                Move move = SudokuMove.createMove(position, boardManager.getBoard(), 3);
+                String result = mController.processMove(move);
+                if (result != null) {
+                    Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent event) {
+                return true;
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getActionMasked();
+        gDetector.onTouchEvent(ev);
+
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mFlingConfirmed = false;
+        } else if (action == MotionEvent.ACTION_DOWN) {
+            mTouchX = ev.getX();
+            mTouchY = ev.getY();
+        } else {
+
+            if (mFlingConfirmed) {
+                return true;
+            }
+
+            float dX = (Math.abs(ev.getX() - mTouchX));
+            float dY = (Math.abs(ev.getY() - mTouchY));
+            if ((dX > SWIPE_MIN_DISTANCE) || (dY > SWIPE_MIN_DISTANCE)) {
+                mFlingConfirmed = true;
+                return true;
+            }
+        }
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return gDetector.onTouchEvent(ev);
+    }
+
+    public void setBoardManager(BoardManager boardManager) {
+        this.boardManager = boardManager;
+        mController.setBoardManager(boardManager);
+    }
+}
