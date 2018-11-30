424a3f46c26787efaedd35f8973dcfc8662a15bd
Mit <mit_rupang@yahoo.ca>
2018-11-30 11:46:32 -0500

    Got rid of unnecessary comments

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/sudoku/SudokuGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/sudoku/SudokuGameActivity.java
@@ -249,11 +249,6 @@ public class SudokuGameActivity extends AppCompatActivity implements Observer {
 
         // save score if game is finished
         if (model.puzzleSolved()) {
-//            Toast.makeText(this, "YOU WIN!", Toast.LENGTH_SHORT).show();
-//            Score score = new Score(user.getUserName(), model.getMovesMade());
-//            GameScoreboard.addScore(this, Board.getHighScoreFile(
-//                    Game.SUDOKU, model.getBoard().getSize()), score);
-            //emulating what Jason did
             String result = model.processGameOver(this, user);
             Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
         }
